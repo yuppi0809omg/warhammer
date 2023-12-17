bc263504fd0f2b3eae22e71d1ef3f52c329840cf
Yuko K <51155709+yuppi0809omg@users.noreply.github.com>
2023-12-17 10:00:52 +0900

    change play function return value structure to include combinations of dice for each set of roles

--- warhammer.js
+++ warhammer.js
@@ -1,51 +1,59 @@
-const play = ()=>{
-// simulate a roll - returns a random number between 1 and 6
-const roll = ()=>{
-    return Math.floor((Math.random()  * 6 + 1))
-}
-
-// roll 20 times to hit
-let firstRoles = 0;
-let hits = 0;
-while(firstRoles < 20){
-    const result = roll();
-
-    // if the result is equal to or biger than 3 AND euqal to or smaller than 5 (3, 4, 5)
-    if(result >= 3 && result <= 5){
-        hits = hits + 1;
+const play = () => {
+    // simulate a roll - returns a random number between 1 and 6
+    const roll = () => {
+        return Math.floor((Math.random() * 6 + 1))
     }
-    // if the result is  6
-    if(result === 6){
-        hits = hits + 3;
+
+    // roll 20 times to hit
+    let firstRoles = 0;
+    let hits = 0;
+    let hitResults = [];
+    while (firstRoles < 20) {
+        const result = roll();
+        hitResults.push(result);
+        // if the result is equal to or biger than 3 AND euqal to or smaller than 5 (3, 4, 5)
+        if (result >= 3 && result <= 5) {
+            hits = hits + 1;
+        }
+        // if the result is  6
+        if (result === 6) {
+            hits = hits + 3;
+        }
+        firstRoles++;
     }
-    firstRoles++;
-}
 
-// rolls [hits] times to wound
-let secondRoles = 0;
-let wounds = 0;
-while(secondRoles < hits){
-    const result = roll();
-    // if the result is equal to or biger than 3
-    if(result >= 3){
-        wounds = wounds + 1;
+    // rolls [hits] times to wound
+    let secondRoles = 0;
+    let wounds = 0;
+    let woundResults = [];
+    while (secondRoles < hits) {
+        const result = roll();
+        woundResults.push(result);
+        // if the result is equal to or biger than 3
+        if (result >= 3) {
+            wounds = wounds + 1;
+        }
+        secondRoles++;
     }
-    secondRoles++;
-}
 
-// rolls [wounds] times to save
-let thirdRoles = 0;
-let damage = 0;
-while(thirdRoles < wounds){
-    const result = roll();
-    // if the result is equal to or smaller than 2
-    if(result <= 2){
-        damage = damage + 1;
+    // rolls [wounds] times to save
+    let thirdRoles = 0;
+    let damage = 0;
+    let damageResults = [];
+    while (thirdRoles < wounds) {
+        const result = roll();
+        damageResults.push(result);
+        // if the result is equal to or smaller than 2
+        if (result <= 2) {
+            damage = damage + 1;
+        }
+        thirdRoles++;
     }
-    thirdRoles++;
-}
-console.log(`hits：${hits}, wounds:${wounds}, damage:${damage}`)
-    return [hits, wounds, damage];
-}
+    console.log(`hits：${hits}, wounds:${wounds}, damage:${damage}`)
+    // return how many hits / wounds / damage it did and also combinations of dice for each set of rolls
+    return {
+        results: [hitResults, woundResults, damageResults],
+        sum: [hits, wounds, damage]
 
-
+    }
+}
